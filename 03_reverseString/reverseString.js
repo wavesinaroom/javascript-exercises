313f77ffd6cd6c65c82970efb37677c1f14d31dc
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-14 11:59:09 -0500

    [Feature] Add exercise three solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  let outputString= new Array(inputString.length);
+  for(let i = 0; i <= inputString.length; i++)
+  {
+    outputString[i] = inputString[inputString.length-i];
+  }
+  return outputString.join("");
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("!123 abc!");
+reverseString("");  
 
 // Do not edit below this line
 module.exports = reverseString;
