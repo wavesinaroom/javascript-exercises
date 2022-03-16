fba68cf200b96f1f7443e34fa243757405136aea
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-16 12:36:53 -0500

    [Feature] Add code changes before refactoring

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,44 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(array, valueOne, valueTwo, valueThree, valueFour) {
+  for (var i = 0; i < array.length; i++) {
+      if((typeof array[i]==typeof valueOne) && (array[i]==valueOne))
+      {
+          array.splice(i, 1);
+      }
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    if((typeof array[i]==typeof valueTwo) && (array[i]==valueTwo))
+    {
+        array.splice(i, 1);
+    }
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    if((typeof array[i]==typeof valueThree) && (array[i]==valueThree))
+    {
+        array.splice(i, 1);
+    }
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    if((typeof array[i]==typeof valueFour) && (array[i]==valueFour))
+    {
+        array.splice(i, 1);
+    }
+  }
+
+
+
+  return array;
 };
+
+removeFromArray([1,2,3,4],3);
+removeFromArray([1,2,3,4],3,2);
+removeFromArray([1, 2, 3, 4], 7, "tacos");
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
