5dec5a07a34861fdab86bfeabadc74694dbb729c
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-17 11:53:18 -0500

    [Feature] Pass exercise 6 test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
 
+const ftoc = function(fahrenheit) {
+  let celsius = (fahrenheit-32)*(5/9);
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fahrenheit = (celsius*(9/5))+32;
+  return fahrenheit;
 };
+
+ftoc(32);
+ctof(0);
 
 // Do not edit below this line
 module.exports = {
