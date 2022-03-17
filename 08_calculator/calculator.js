590b536545ce85ceba1b47f8a92e3fb06ff1f98a
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-17 12:34:18 -0500

    [Feature] Pass exercise eight tests two and three

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,4 +1,7 @@
 const add = function(inputOne, inputTwo) {
+	if (inputOne<0||inputTwo<0) {
+		return "ERROR";
+	}
 	result = inputOne + inputTwo;
 	return result;
 };
@@ -25,7 +28,8 @@ const factorial = function() {
 
 //Add funtion calls
 add(0,0);
-
+add(2,2);
+add(2,6);
 // Do not edit below this line
 module.exports = {
   add,
--- 08_calculator/calculator.spec.js
+++ 08_calculator/calculator.spec.js
@@ -5,12 +5,12 @@ describe('add', () => {
 		expect(calculator.add(0,0)).toBe(0);
 	});
 
-	test.skip('adds 2 and 2', () => {
+	test('adds 2 and 2', () => {
 		expect(calculator.add(2,2)).toBe(4);
 	});
 
-	test.skip('adds positive numbers', () => {
-		expect(calculator.add(2,6)).toBe(8);
+	test('adds positive numbers', () => {
+		expect(calculator.add(2,-6)).toBe("ERROR");
 	});
 });
 
