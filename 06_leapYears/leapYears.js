9260c0068af5a827f16ee6fb1d4e614b3da8ecaf
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-17 11:22:06 -0500

    [Feature] Pass exercise 6 test 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,22 @@
 const leapYears = function(year) {
   let isLeapYear = false;
+
   if (year%4==0) {
-      isLeapYear = true;
-    }
+    isLeapYear = true;
+  }
+  if (year%400==0) {
+    isLeapYear = true;
+  }
+  if (year%100==0) {
+    isLeapYear = false;
+  }
   return isLeapYear;
 };
 
 leapYears(1996);
 leapYears(1997);
 leapYears(34992);
+leapYears(1900);
 
 // Do not edit below this line
 module.exports = leapYears;
