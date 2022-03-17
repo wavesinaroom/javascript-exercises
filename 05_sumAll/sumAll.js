8aa5057b9b90b26cc669fe616bf5fdd44eb1d30b
Juan Jáuregui <juan@wavesinaroom.com>
2022-03-17 10:14:40 -0500

    [Fix] Delete double commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+  let result=0;
+  if (firstNumber<0||secondNumber<0) {
+    return "ERROR"; 
+  }
 
+  if (secondNumber>firstNumber) {
+    for (var i = firstNumber; i <= (secondNumber+1)-firstNumber; i++) {
+      result+=i;
+    }
+  }else {
+    for (var i = secondNumber; i <= (firstNumber+1)-secondNumber; i++) {
+      result+=i;
+    }
+  }
+  return result;
 };
+
+sumAll(1,4);
+sumAll(1,4000);
+sumAll(123,1);
 
 // Do not edit below this line
 module.exports = sumAll;
