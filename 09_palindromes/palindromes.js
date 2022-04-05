e9527d251f9b1f2d7e4bef4592a77e3e75701a53
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-05 11:19:54 -0500

    [Feature] Pass exercise 09-palindromes test 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
 
-};
+  let isPalindrome = false;
+
+  let palindrome = inputString.split('').reverse('').join('');
+
+   if (inputString===palindrome)
+   {
+     isPalindrome = true;
+   }
+   return isPalindrome;
+ }
+
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
