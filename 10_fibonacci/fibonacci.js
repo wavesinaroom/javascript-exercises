0b93bb8706f1c8adaf175b8f843256177b172c80
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-07 09:48:34 -0500

    [Feature] Pass fibonacci test one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(term) {
 
+  let second = 1;
+  let first = 0;
+  let result = 0;
+
+  if(term==0)
+  {
+    return first;
+  }
+
+  for (var i = 2; i <= term ; i++) {
+    result = second +first;
+    first = second;
+    second = result;
+    console.log(result);
+  }
+
+  return result;
 };
 
 // Do not edit below this line
