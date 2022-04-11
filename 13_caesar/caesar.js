bc6721c05ae64fd2899cf33bed806e12db57e663
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-11 16:18:09 -0500

    [Feature] Pass caesar cipher test 2

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,13 +1,12 @@
 const caesar = function(inputMessage, shiftFactor) {
-  //Convert to unicode
-  let char = new Array(inputMessage.length)
-  let outputMessage = new Array(inputMessage.length)
+  let char = new Array(inputMessage.length);
+  let outputMessage = new Array(inputMessage.length);
   for (var i = 0; i < inputMessage.length; i++) {
     char[i] = inputMessage.charCodeAt(i);
     char[i] += shiftFactor;
-    outputMessage = String.fromCharCode(char[i]);
+    outputMessage[i] = String.fromCharCode(char[i]);
   }
-  return outputMessage;
+  return outputMessage.join('');
 };
 
 // Do not edit below this line
--- 13_caesar/caesar.spec.js
+++ 13_caesar/caesar.spec.js
@@ -3,7 +3,7 @@ const caesar = require('./caesar')
 test('works with single letters', () => {
   expect(caesar('A', 1)).toBe('B');
 });
-test.skip('works with words', () => {
+test('works with words', () => {
   expect(caesar('Aaa', 1)).toBe('Bbb');
 });
 test.skip('works with phrases', () => {
