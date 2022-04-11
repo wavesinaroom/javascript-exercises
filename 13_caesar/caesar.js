b60c5645900db40c81327d333421a542e7aa4b40
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-11 15:57:35 -0500

    [Feature] Pass test one caesar cipher

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,13 @@
-const caesar = function() {
-
+const caesar = function(inputMessage, shiftFactor) {
+  //Convert to unicode
+  let char = new Array(inputMessage.length)
+  let outputMessage = new Array(inputMessage.length)
+  for (var i = 0; i < inputMessage.length; i++) {
+    char[i] = inputMessage.charCodeAt(i);
+    char[i] += shiftFactor;
+    outputMessage = String.fromCharCode(char[i]);
+  }
+  return outputMessage;
 };
 
 // Do not edit below this line
