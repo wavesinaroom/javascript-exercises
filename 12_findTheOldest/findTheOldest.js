fb16f6d378b5053543804bba0e3f15a23fd99aca
Juan Jáuregui <juan@wavesinaroom.com>
2022-04-11 15:04:22 -0500

    [Feature] Pass find the oldest tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
-};
+  let oldest = people[0];
+  let currentDate = new Date();
+
+  for (var i = 0 ; i < people.length; i++) {
+
+    if(typeof people[i].yearOfDeath === 'undefined')
+    {
+      people[i].yearOfDeath = currentDate.getFullYear();
+    }
+
+    if((oldest.yearOfDeath-oldest.yearOfBirth)<(people[i].yearOfDeath-people[i].yearOfBirth))
+    {
+      oldest = people[i];
+    }
+  }
+  return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
